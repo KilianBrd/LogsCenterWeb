071e9f57a16863d8c7f2bc043795fc478bd674ac
fabienmcll <fabienmclll@gmail.com>
2025-07-16 09:35:38 +0000

    Installation de tailwindCSS.

--- src/View/logs.php
+++ src/View/logs.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link href="./output.css" rel="stylesheet">
     <title>Logs simples</title>
 </head>
 <body>
-    <h1>Liste des logs statiques</h1>
+    <h1 class="text-xl font-bold">Liste des logs statiques</h1>
     <ul>
         <?php foreach ($logs as $log): ?>
             <li><?= htmlspecialchars($log['message']) ?></li>
